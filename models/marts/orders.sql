68b68d78435aee0b8592eeecf3d83f83db310e90
anna.barr@fivetran.com <anna.barr@fivetran.com>
2023-04-13 15:49:12 +0000

    test

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -1,23 +1,7 @@
-with orders as (
+with
+    orders as (select * from {{ ref("int_order_payments_pivoted") }}),
+    customers as (select * from {{ ref("int_customer_order_history_joined") }}),
+    final as (select * from orders left join customers using (customer_id))
 
-    select * from {{ ref('int_order_payments_pivoted') }}
-
-)
-,
-customers as (
-
-    select * from {{ ref('int_customer_order_history_joined') }}
-
-)
-,
-final as (
-
-    select 
-        *
-    from orders 
-    left join customers using (customer_id)
-
-)
-
-select * from final
-
+select *
+from
